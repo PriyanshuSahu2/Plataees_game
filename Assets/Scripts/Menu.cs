6ff09312fd281287efff7114207db78ae132cbe9
Priyanshu <sahupriyanshu108@gmail.com>
2022-04-04 23:30:35 +0530

    pauseMenu Added

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] public GameObject menu;
+
+    private void Start()
+    {
+        menu.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menu.activeSelf)
+            {
+                menu.SetActive(false);
+            Time.timeScale = 1f;
+            }
+            else
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+                menu.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        menu.SetActive(false);
+    }
+    public void exit()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+}
