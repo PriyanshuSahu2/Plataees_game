88df9c8611988f7953a58463ca9133703512b79d
Priyanshu <sahupriyanshu108@gmail.com>
2022-09-06 22:33:44 +0530

    Added
    :-1: Added Teleportation to every Garden (DIst1)
    
    :- Level all Houses
    
    :- Added Minimap for Player
    
    :- Fixed Some Menu Bugs

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+    private void Update()
+    {
+        
+    }
+}
