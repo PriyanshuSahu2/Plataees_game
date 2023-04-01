22649b21fa83f94bd1a4fceee2694c11d0bcde02
Priyanshu <sahupriyanshu108@gmail.com>
2023-04-01 18:25:02 +0530

    D4_Completed

--- Assets/Scripts/Axis.cs
+++ Assets/Scripts/Axis.cs
@@ -2,35 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Axis : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] GameObject xAxis;
     [SerializeField] GameObject yAxis;
     public GameObject myPlayer;
-    [SerializeField] float x = 3.35f;
-    [SerializeField] float y = 3.17f;
     [SerializeField] Image image;
-  
-    void Start()
+
+    private float levelWidth = 2370.44f;
+    private float levelHeight = 2453.12f;
+    private float mapWidth = 1058f;
+    private float mapHeight = 996f;
+
+    private void Update()
     {
-       
+        // Calculate the scale factor for the map
+        float xScale = mapWidth / levelWidth;
+        float yScale = mapHeight / levelHeight;
 
+        // Calculate the position of the player relative to the map
+        float playerX = myPlayer.transform.position.x / levelWidth * mapWidth;
+        float playerY = myPlayer.transform.position.z / levelHeight * mapHeight;
 
-    }
-
-    private void OnEnable()
-    {
-
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        // Debug.Log($"{player.transform.position.x - xAxis.transform.position.x} , {player.transform.position.z - yAxis.transform.position.z}");
-        if (myPlayer)
-        {
-            Debug.Log(myPlayer.transform.position.x- xAxis.transform.position.x);
-            image.rectTransform.anchoredPosition = new Vector2((myPlayer.transform.position.z - yAxis.transform.position.z) / y, (myPlayer.transform.position.x - xAxis.transform.position.x) / x);
-        }
+        // Set the position of the map icon
+        image.rectTransform.anchoredPosition = new Vector2(playerX, playerY);
     }
 }
