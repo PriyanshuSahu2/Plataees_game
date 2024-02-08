3f7f5328f872112b7b7ff0e0998a2084fd361635
Priyanshu <sahupriyanshu108@gmail.com>
2024-02-08 16:19:02 +0530

    updates

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -31,7 +31,7 @@ public class RoomManager : MonoBehaviourPunCallbacks
     }
     void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
     {
-        if (scene.buildIndex == 1|| scene.buildIndex == 2|| scene.buildIndex == 3|| scene.buildIndex == 4|| scene.buildIndex == 5)
+        if (scene.buildIndex !=0)
         {
             GameObject gb = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);
         }
